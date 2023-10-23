1c710732b35e560dd13ad08ac417c5720c23f58c
Kim JeongHyeon <jeonghyeon.kim@cosmedia.co.jp>
2023-10-23 13:19:18 +0900

    new branch practice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import "./practice.css";
+import { getMouseEventOptions } from '@testing-library/user-event/dist/utils';
 
 function MkList(props) {
   const lis = [];
@@ -13,7 +14,7 @@ function MkList(props) {
         <div className="div__info">
           <div className="div__text--title">{t.title}</div>
           <div className="div__text--hour">{t.hour}hurs</div>
-          <div className="div__text--lastAccess">Yesterday - {t.contents}hrs</div>
+          <div className="div__text--lastAccess">{t.contents}hrs</div>
         </div>
       </div>
     );
@@ -27,14 +28,43 @@ function MkList(props) {
 
 
 export default function App() {  
+  const [mode, setMode] = useState('weekly')
+  const [click, setClick] = useState('weekly')
   const daily = [
-    {color : "div__header--color1", classImage : "div__header--image1", title : "work", hour : "5", contents : "7" },
-    {color : "div__header--color2", classImage : "div__header--image2", title : "Play",  hour : "1", contents : "2" },
-    {color : "div__header--color3", classImage : "div__header--image3", title : "Study",  hour : "0", contents : "1" },
-    {color : "div__header--color4", classImage : "div__header--image4", title : "Exercise",  hour : "1", contents : "1" },
-    {color : "div__header--color5", classImage : "div__header--image5", title : "Social",  hour : "1", contents : "3" },
-    {color : "div__header--color6", classImage : "div__header--image6", title : "Self Care",  hour : "0", contents : "1" }
+    {color : "div__header--color1", classImage : "div__header--image1", title : "work", hour : "5", contents : "Yesterday - 7" },
+    {color : "div__header--color2", classImage : "div__header--image2", title : "Play",  hour : "1", contents : "Yesterday - 2" },
+    {color : "div__header--color3", classImage : "div__header--image3", title : "Study",  hour : "0", contents : "Yesterday - 1" },
+    {color : "div__header--color4", classImage : "div__header--image4", title : "Exercise",  hour : "1", contents : "Yesterday - 1" },
+    {color : "div__header--color5", classImage : "div__header--image5", title : "Social",  hour : "1", contents : "Yesterday - 3" },
+    {color : "div__header--color6", classImage : "div__header--image6", title : "Self Care",  hour : "0", contents : "Yesterday - 1" }
   ];
+  const weekely = [
+    {color : "div__header--color1", classImage : "div__header--image1", title : "work", hour : "32", contents : "Last Week - 36"},
+    {color : "div__header--color2", classImage : "div__header--image2", title : "Play",  hour : "10", contents : "Last Week - 8" },
+    {color : "div__header--color3", classImage : "div__header--image3", title : "Study",  hour : "4", contents : "Last Week - 7" },
+    {color : "div__header--color4", classImage : "div__header--image4", title : "Exercise",  hour : "4", contents : "Last Week - 5" },
+    {color : "div__header--color5", classImage : "div__header--image5", title : "Social",  hour : "5", contents : "Last Week - 10" },
+    {color : "div__header--color6", classImage : "div__header--image6", title : "Self Care",  hour : "2", contents : "Last Week - 2" }
+  ];
+  const monthly = [
+    {color : "div__header--color1", classImage : "div__header--image1", title : "work", hour : "103", contents : "Last Month - 128" },
+    {color : "div__header--color2", classImage : "div__header--image2", title : "Play",  hour : "23", contents : "Last Month - 29" },
+    {color : "div__header--color3", classImage : "div__header--image3", title : "Study",  hour : "13", contents : "Last Month - 19" },
+    {color : "div__header--color4", classImage : "div__header--image4", title : "Exercise",  hour : "11", contents : "Last Month - 18" },
+    {color : "div__header--color5", classImage : "div__header--image5", title : "Social",  hour : "21", contents : "Last Month - 23" },
+    {color : "div__header--color6", classImage : "div__header--image6", title : "Self Care",  hour : "7", contents : "Last Month - 11" }
+  ];
+  let contents = null;
+  if (mode === "daily"){
+    contents = <MkList listName = {daily} ></MkList>
+  }else if(mode === "weekly"){
+    contents = <MkList listName = {weekely} ></MkList>
+  }else if(mode === "monthly"){
+    contents = <MkList listName = {monthly} ></MkList>
+  }
+
+
+
   
   return (
     <>
@@ -50,22 +80,35 @@ export default function App() {
 
               <div className="div__profile--list">
                 <ul className='div__profile--ul'>
-                  <li className='div__profile--li' style={{color : "#ccc"}}>Daily</li>
-                  <li className='div__profile--li'>Weekly</li>
-                  <li className='div__profile--li'>Monthly</li>
+                  <li className={click === 'daily' ? 'div__profile--li font__click' : 'div__profile--li' } onClick = {() => {
+                    if(mode !== 'daily'){
+                      setClick('daily')
+                      setMode('daily')
+                    }
+                  }}>Daily</li>
+                  <li className={click === 'weekly' ? 'div__profile--li font__click' : 'div__profile--li' } onClick = {() => {
+                    if(mode !== 'weekly'){
+                      setClick('weekly')
+                      setMode('weekly')
+                    }
+                  }}>Weekly</li>
+                  <li className={click === 'monthly' ? 'div__profile--li font__click' : 'div__profile--li'} onClick = {() => {
+                    if(mode !== 'monthly'){
+                      setClick('monthly')
+                      setMode('monthly')
+                    }
+                  }}>Monthly</li>
                 </ul>
               </div>
             
           </div>
 
           <div className='div__item'>
-
-            <MkList listName = {daily} ></MkList>
-
+            {contents}
           </div>
         </div>
         <div>
-          <p className='div__self'>Challenge by <a href = "https://www.frontendmentor.io/?ref=challenge/">Frontend Mentor.</a> Coded by <a href = "#">Jeong Hyeon Kim.</a></p>
+          <p className='div__self'>Challenge by <a href = "https://www.frontendmentor.io/?ref=challenge/">Frontend Mentor.</a> Coded by <a href = "/">Jeong Hyeon Kim.</a></p>
         </div>
       </div>    
     </>
